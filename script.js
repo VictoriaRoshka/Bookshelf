20f49a8774c66659cd4bdd7e240e6b34f7b8a50d
VictoriaRoshka <kawaii0109@mail.ru>
2022-11-12 15:56:52 +0300

    добавление книги

--- script.js
+++ script.js
@@ -52,3 +52,70 @@ books.forEach(function(book) {
       </div>
     </div>`
 })
+
+function renderBooks(){
+    bookBox.innerHTML = ""
+    books.forEach(function(book) {
+        bookBox.innerHTML += `
+        <div class="bookBox__item">
+         <div>${book.image}</div>
+         <div class="bookBox__item_title">${book.title}</div>
+         <div class="bookBox__item_year">${book.year}</div>
+         <div class="bookBox__item_author">${book.author}</div>
+         <div class="bookBox__item_buttons">
+                <button class="btn btn_change">Изменить</button>
+                <button class="btn btn_delete">Удалить</button>
+          </div>
+        </div>`
+    })
+
+}
+
+function clearForm(){
+    document.getElementById('title').value = ""
+    document.getElementById('author').value = ""
+    document.getElementById('year').value = ""
+    document.getElementById('image').value = ""
+}
+
+let isOpen = false
+function addMenu(){
+    const addContainer = document.getElementById('add-container')
+    const addButton = document.getElementById('bookMenu')
+
+    if (isOpen){
+
+        addContainer.style.display = "none"
+
+        isOpen = false
+    } else {
+        
+        addContainer.style.display = "flex"
+
+        isOpen = true
+
+    }
+}
+
+function addBook(){
+    const btnAdd = document.getElementById('addBook')
+    const titleBook = document.getElementById('title').value
+    const authorBook = document.getElementById('author').value
+    const yearBook = document.getElementById('year').value
+    const imageBook = document.getElementById('image').value
+
+
+    const book = {
+        title: titleBook,
+        author: authorBook,
+        year: yearBook,
+        image: imageBook
+    }
+    books.push(book)
+
+    renderBooks()
+    clearForm()
+    
+}
+
+
