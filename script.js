3047ba30469f4b5abcc099311edad2b0e6b3c145
VictoriaRoshka <kawaii0109@mail.ru>
2022-11-12 17:51:52 +0300

    удаление книг

--- script.js
+++ script.js
@@ -3,56 +3,47 @@ const books = [
         title:'Портрет Дориана Грея',
         author: 'Оскар Уайльд',
         year: '1890',
-        image:'<img class="bookBox__item_image" src="images/дориан.jpg" alt=""/>'
+        image:'<img class="bookBox__item_image" src="images/дориан.jpg" alt=""/>',
+        id: 1
     },
     {
         title:'Граф Монте-Кристо',
         author: 'Александр Дюма',
         year: '1844',
-        image:'<img class="bookBox__item_image" src="images/граф.jpg" alt=""/>'
+        image:'<img class="bookBox__item_image" src="images/граф.jpg" alt=""/>',
+        id: 2
     },
     {
         title:'До встречи с тобой',
         author:'Джоджо Мойес',
         year: '2012',
         image: '<img class="bookBox__item_image" src="images/до встречи.jpg" alt=""/>',
+        id: 3
     },
     {
         title:'После тебя',
         author:'Джоджо Мойес',
         year:'2015',
-        image:'<img class="bookBox__item_image" src="images/после тебя.jpg" alt=""/>'
+        image:'<img class="bookBox__item_image" src="images/после тебя.jpg" alt=""/>',
+        id: 4
     },
     {
         title:'Все та же я',
         author:'Джоджо Мойес',
         year:'2018',
-        image:'<img class="bookBox__item_image" src="images/все та же я.jpg" alt=""/>'
+        image:'<img class="bookBox__item_image" src="images/все та же я.jpg" alt=""/>',
+        id: 5
     },
     {
         title:'Первое правило волшебника',
         author:'Терри Гудкайнд',
         year:'1994',
-        image:'<img class="bookBox__item_image" src="images/Первое правило.jpg" alt=""/>'
+        image:'<img class="bookBox__item_image" src="images/Первое правило.jpg" alt=""/>',
+        id: 6
     },
     
 ]
 let bookBox = document.getElementById('container')
-
-books.forEach(function(book) {
-    bookBox.innerHTML += `
-    <div class="bookBox__item">
-     <div>${book.image}</div>
-     <div class="bookBox__item_title">${book.title}</div>
-     <div class="bookBox__item_year">${book.year}</div>
-     <div class="bookBox__item_author">${book.author}</div>
-     <div class="bookBox__item_buttons">
-            <button class="btn btn_change">Изменить</button>
-            <button class="btn btn_delete">Удалить</button>
-      </div>
-    </div>`
-})
-
 function renderBooks(){
     bookBox.innerHTML = ""
     books.forEach(function(book) {
@@ -64,11 +55,21 @@ function renderBooks(){
          <div class="bookBox__item_author">${book.author}</div>
          <div class="bookBox__item_buttons">
                 <button class="btn btn_change">Изменить</button>
-                <button class="btn btn_delete">Удалить</button>
+                <button class="btn btn_delete" onclick='deleteBook(${book.id})'>Удалить</button>
           </div>
         </div>`
     })
+}
 
+function deleteBook(id){
+    const book = books.find((b) => {
+        return b.id === id
+    })
+    const bookIndex = books.indexOf(book)
+
+    books.splice(bookIndex, 1)
+
+    renderBooks()
 }
 
 function clearForm(){
@@ -79,7 +80,7 @@ function clearForm(){
 }
 
 let isOpen = false
-function addMenu(){
+function addBook(){
     const addContainer = document.getElementById('add-container')
     const addButton = document.getElementById('bookMenu')
 
@@ -97,7 +98,7 @@ function addMenu(){
     }
 }
 
-function addBook(){
+function saveBook(){
     const btnAdd = document.getElementById('addBook')
     const titleBook = document.getElementById('title').value
     const authorBook = document.getElementById('author').value
@@ -117,5 +118,6 @@ function addBook(){
     clearForm()
     
 }
+renderBooks()
 
 
